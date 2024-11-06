ca01ce2ba60b8317bf541c487c9b01208cadf22a
Chris Panetta <cv.panetta@gmail.com>
2024-11-05 23:33:14 -0500

    updated files

--- mkdocker.bat
+++ mkdocker.bat
@@ -4,5 +4,5 @@ docker stop afroz-wsp2
 docker rm afroz-wsp2
 cd C:\Users\Administrator\Documents\Afroz\Afroz-WSP2
 docker build -t afroz-wsp2 .
-docker run -d -p 3336:4000 --name afroz-wsp2 afroz-wsp2
+docker run -d -p 3335:4000 --name afroz-wsp2 afroz-wsp2
 pause
